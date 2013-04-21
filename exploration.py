6c5d40300faf64b44515032ccf605dab64d6e305
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-21 22:11:21 +0100

    problem: investigation question

---
+++ exploration.py
@@ -0,0 +1,9 @@
+from adjacent_digits import count_non_adjacent_digits
+__author__ = 'julz'
+
+print count_non_adjacent_digits(4)
+# 4
+print count_non_adjacent_digits(5)
+# 5
+print count_non_adjacent_digits(10)
+# 8
