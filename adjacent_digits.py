38a3d20912aa3330f008636ad7dc3ba71c2854cf
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-21 21:34:03 +0100

    init commit

---
+++ adjacent_digits.py
@@ -0,0 +1,11 @@
+__author__ = 'julz'
+# Think of binary numbers: sequences of 0's and 1's.
+# How many n-digit binary numbers are there that don't have two adjacent 1 bits?
+# For example, for three-digit numbers,
+# five of the possible eight combinations meet the criteria:
+# 000, 001, 010, XXX011, 100, 101, XXX110, XXX111.
+
+# What is the number for sequences of length 4, 5, 10, n?
+
+# http://codekata.pragprog.com/2007/01/code_kata_fifte.html#more
+
