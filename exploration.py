ade3066426cbb4bd2de034915fb9ad6b21fae2cc
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-21 22:13:29 +0100

    more investigation

--- exploration.py
+++ exploration.py
@@ -1,9 +1,33 @@
 from adjacent_digits import count_non_adjacent_digits
 __author__ = 'julz'
 
-print count_non_adjacent_digits(4)
+# print count_non_adjacent_digits(4)
 # 4
-print count_non_adjacent_digits(5)
+# print count_non_adjacent_digits(5)
 # 5
-print count_non_adjacent_digits(10)
+# print count_non_adjacent_digits(10)
 # 8
+
+for i in range(0,21):
+    print '%s : %s' %(i, count_non_adjacent_digits(i))
+#   0 : 1
+#   1 : 2
+#   2 : 3
+#   3 : 3
+#   4 : 4
+#   5 : 5
+#   6 : 5
+#   7 : 5
+#   8 : 6
+#   9 : 7
+#   10 : 8
+#   11 : 8
+#   12 : 8
+#   13 : 8
+#   14 : 8
+#   15 : 8
+#   16 : 9
+#   17 : 10
+#   18 : 11
+#   19 : 11
+#   20 : 12
