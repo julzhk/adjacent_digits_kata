e8af086f99f1faa55e8559b3377a3762c56f33da
Julian Harley <julz@Julians-MacBook-Pro.local>
2013-04-21 21:43:45 +0100

    more coverage

--- tests.py
+++ tests.py
@@ -5,6 +5,9 @@ class test_binary_digits(unittest.TestCase):
     def test_adjacent_digits(self):
         self.assertFalse(has_adjacent_digits('10'))
         self.assertTrue(has_adjacent_digits('11'))
+        self.assertTrue(has_adjacent_digits('0110'))
+        self.assertFalse(has_adjacent_digits('00'))
+        self.assertFalse(has_adjacent_digits('0'))
 
 
 
